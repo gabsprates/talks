e9d6781f6f7661a4652ceec91d1e642e4488b9a2
Gabriel Prates <gabsprates@gmail.com>
2021-09-22 11:00:31 -0300

    Remove unused tests

--- expect-you-to-test-jest/playlist/index.todo.test.js
+++
@@ -1,9 +0,0 @@
-const Playlist = require("./index");
-
-describe("testing playlist", () => {
-  it("should to shuffle playlist", () => {});
-
-  describe("resquests", () => {});
-
-  describe("should be 5", () => {});
-});
--- expect-you-to-test-jest/setup.todo.js
+++
@@ -1,7 +0,0 @@
-// arrange
-// act
-// assert
-
-const it = (msg, fn) => {};
-const expect = received => {};
-const describe = (msg, fn) => {};
