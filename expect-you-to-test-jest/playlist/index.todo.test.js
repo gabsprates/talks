31e62e0963c2bafb3087bb9281377dcb56161619
Gabriel Prates <gabsprates@gmail.com>
2021-09-22 09:37:55 -0300

    Add expect-you-to-test-jest

---
+++ expect-you-to-test-jest/playlist/index.todo.test.js
@@ -0,0 +1,9 @@
+const Playlist = require("./index");
+
+describe("testing playlist", () => {
+  it("should to shuffle playlist", () => {});
+
+  describe("resquests", () => {});
+
+  describe("should be 5", () => {});
+});
---
+++ expect-you-to-test-jest/requests/__mocks__/index.js
@@ -0,0 +1,11 @@
+const request = {
+  getPlaylist: () =>
+    Array(8)
+      .fill(null)
+      .map((_, i) => ({
+        track: i,
+        title: `song ${i}`
+      }))
+};
+
+module.exports = request;
