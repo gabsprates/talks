629c9cc63a9a2ea800481852123728ead5ea9311
Gabriel Prates <gabsprates@gmail.com>
2021-11-25 17:10:44 -0300

    refactor: user component

--- cra-react-testing/src/user/__tests__/index.test.tsx
+++ cra-react-testing/src/user/__tests__/index.test.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { User } from "../index";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
 describe("Component: User", () => {
   it("should render without errors", () => {
-    const wrapper = render(<User name="prates" github="gabsprates" />);
+    render(<User name="prates" github="gabsprates" />);
 
-    expect(wrapper.container.outerHTML).toMatchSnapshot();
-    expect(wrapper.getByText("prates"));
+    expect(screen.getByText("prates")).toBeInTheDocument();
+    expect(screen.getByText("gabsprates")).toBeInTheDocument();
   });
 });
